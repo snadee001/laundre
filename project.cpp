91d5293a461bddfa46992df47b6486bcd4d20c65
albertcai101 <albertcai.paloalto@gmail.com>
2024-05-30 18:59:59 -0700

    corrected simviz paths

--- project.cpp
+++ project.cpp
@@ -49,7 +49,7 @@ using namespace Sai2Primitives;
 using namespace ruckig;
 
 // config file names and object names
-const string robot_file = "${HW_FOLDER}/laundre/panda_arm_hand.urdf";
+const string robot_file = "${HW_FOLDER}/laundre/panda_arm_sphere.urdf";
 
 Vector2d grasp(bool is_grasp) {
     if (is_grasp) return Vector2d(0.05, -0.05);
