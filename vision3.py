db08dbee13f1a8ef53fca20a169bef689d41086a
adelsuh <adelsuh@naver.com>
2024-06-03 19:27:16 -0700

    Accounted for reversed axes

--- vision3.py
+++ vision3.py
@@ -15,42 +15,42 @@ def detect_shirt_by_color(frame, lower_color, upper_color):
 def find_right_bottom_corner(mask):
     width, height = mask.shape
     mask_list = [(x, y) for x in range(width) for y in range(height) if mask[x][y]]
-    mask_list_edited = [(height - y) + (width - x) for (x, y) in mask_list]
+    mask_list_edited = [x + y for (x, y) in mask_list]
     mask_dict = {mask_list[i]: mask_list_edited[i] for i in range(len(mask_list))}
-    res = min(mask_dict, key=mask_dict.get)
-    return (res[1], res[0])
+    res = max(mask_dict, key=mask_dict.get)
+    return res
 
 def find_left_bottom_corner(mask):
     width, height = mask.shape
     mask_list = [(x, y) for x in range(width) for y in range(height) if mask[x][y]]
-    mask_list_edited = [(height - y) + x for (x, y) in mask_list]
+    mask_list_edited = [x - y for (x, y) in mask_list]
     mask_dict = {mask_list[i]: mask_list_edited[i] for i in range(len(mask_list))}
     res = max(mask_dict, key=mask_dict.get)
-    return (res[1], res[0])
+    return res
 
 def find_right_sleeve_top(mask):
     width, height = mask.shape
     mask_list = [(x, y) for x in range(width) for y in range(height) if mask[x][y]]
-    mask_list_edited = [x for (x, y) in mask_list]
+    mask_list_edited = [y for (x, y) in mask_list]
     mask_dict = {mask_list[i]: mask_list_edited[i] for i in range(len(mask_list))}
     res = max(mask_dict, key=mask_dict.get)
-    return (res[1], res[0])
+    return res
 
 def find_left_sleeve_top(mask):
     width, height = mask.shape
     mask_list = [(x, y) for x in range(width) for y in range(height) if mask[x][y]]
-    mask_list_edited = [x for (x, y) in mask_list]
+    mask_list_edited = [y for (x, y) in mask_list]
     mask_dict = {mask_list[i]: mask_list_edited[i] for i in range(len(mask_list))}
     res = min(mask_dict, key=mask_dict.get)
-    return (res[1], res[0])
+    return res
 
 def find_sleeve(mask, bottom, sleeve_top):
-    width, height = mask.shape
-    inner_x = bottom[0]+(sleeve_top[0]-bottom[0])*0.05
-    line = [y for y in range(height) if mask[inner_x][y]]
-    y_mid = (min(line)+max(line))/2
-    sleeve_mid_outer = (sleeve_top[0], y_mid)
-    sleeve_mid_inner = (bottom[0], y_mid)
+    width, _ = mask.shape
+    inner_y = int(bottom[1]+(sleeve_top[1]-bottom[1])*0.05)
+    line = [x for x in range(width) if mask[x][inner_y]]
+    x_mid = int((min(line)+max(line))/2)
+    sleeve_mid_outer = (x_mid, sleeve_top[1])
+    sleeve_mid_inner = (x_mid, bottom[1])
     return sleeve_mid_outer, sleeve_mid_inner
 
 class WebcamProcessor:
@@ -70,26 +70,35 @@ class WebcamProcessor:
 
             shirt_contour, mask = detect_shirt_by_color(frame, lower_color, upper_color)
             if shirt_contour is not None and self.start_processing:
+                points = []
+
                 leftbottom = find_left_bottom_corner(mask)
+                points.append(leftbottom)
+
                 rightbottom = find_right_bottom_corner(mask)
+                points.append(rightbottom)
+
                 left_sleeve_top = find_left_sleeve_top(mask)
                 right_sleeve_top = find_right_sleeve_top(mask)
                 left_sleeve_outer, left_sleeve_inner = find_sleeve(mask, leftbottom, left_sleeve_top)
+                points.append(left_sleeve_outer)
+                points.append(left_sleeve_inner)
                 right_sleeve_outer, right_sleeve_inner = find_sleeve(mask, rightbottom, right_sleeve_top)
+                points.append(right_sleeve_outer, right_sleeve_inner)
 
-                shirt_height = left_sleeve_top[1]-leftbottom[1]
-                shirt_width = rightbottom[0]-leftbottom[0]
-                leftbottom_target = (leftbottom[0]+0.25*shirt_width, leftbottom[1]-shirt_height/6)
-                rightbottom_target = (rightbottom[0]-0.25*shirt_width, rightbottom[1]-shirt_height/6)
-                midbottom = ((leftbottom[0]+rightbottom[0])/2, (leftbottom[1]+rightbottom[1])/2)
-                mid2 = (midbottom[0], midbottom[1]-shirt_height/3)
-                mid3 = (midbottom[0], midbottom[1]-2*shirt_height/3)
-                third = (left_sleeve_inner[0]+shirt_width/3, left_sleeve_inner[1])
-                third2 = (left_sleeve_inner[0]+2*shirt_width/3, left_sleeve_inner[1])
+                shirt_height = leftbottom[0] - left_sleeve_top[0]
+                shirt_width = rightbottom[1]-leftbottom[1]
+                leftbottom_target = (int(leftbottom[0]-shirt_height/6), int(leftbottom[1]+0.25*shirt_width))
+                rightbottom_target = (int(rightbottom[0]-shirt_height/6), int(rightbottom[1]-0.25*shirt_width))
+                midbottom = (int((leftbottom[0]+rightbottom[0])/2), int((leftbottom[1]+rightbottom[1])/2))
+                mid2 = (midbottom[0], midbottom[1])
+                mid3 = (midbottom[0]-shirt_height/3, midbottom[1])
+                third = (left_sleeve_inner[0], left_sleeve_inner[1]+shirt_width/3)
+                third2 = (left_sleeve_inner[0], left_sleeve_inner[1]+2*shirt_width/3)
 
                 for point in [leftbottom, rightbottom, left_sleeve_outer, left_sleeve_inner, right_sleeve_outer, right_sleeve_inner, leftbottom_target, rightbottom_target, midbottom, mid2, mid3, third, third2]:
                     cv2.circle(frame, point, 50, (0, 0, 255), -1)
-                    
+
             # Display both the original frame and the mask
             combined_image = np.hstack((frame, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)))
             cv2.imshow('Webcam Frame and Mask', combined_image)
