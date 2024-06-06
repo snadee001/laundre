7cbe8274e6ef695bc20b315df9042576bf7e6695
chongwilliam <wmchong@stanford.edu>
2024-06-06 03:40:15 -0700

    sharing code...

--- project.cpp
+++ project.cpp
@@ -34,12 +34,13 @@ enum Part {
 };
 
 enum Action {
-    POSTURE,
-    MOTION,
+    VISION,
     REACH,
     SLIP,
     FLIP,
-    RISE
+    RISE, 
+    // LEFT,
+    // RIGHT,
 };
 
 // for handling ctrl+c and interruptions properly
@@ -53,15 +54,15 @@ using namespace Eigen;
 using namespace Sai2Primitives;
 
 // config file names and object names
-const string robot_file = "${HW_FOLDER}/laundre/panda/panda_arm.urdf";
+const string robot_file = "${HW_FOLDER}/laundre/panda/panda_arm_spatula.urdf";
 const bool flag_simulation = false;
 
 Matrix3d ori(Vector3d cur, Vector3d target) {
     Matrix3d result = Matrix3d::Zero();
-    result(2, 2) = -1.0;
+    result(2, 2) = 1.0;
     Vector2d vec = (target - cur).normalized().head(2);
-    result(0, 0) = vec(0);
-    result(1, 0) = vec(1);
+    result(0, 0) = -vec(0);
+    result(1, 0) = -vec(1);
     result(0, 1) = vec(1);
     result(1, 1) = -vec(0);
     return result;
@@ -89,9 +90,9 @@ int main(int argc, char** argv) {
 	}
 
     // computer vision 
-    float camera_height = 0.08;
-    //int vision = 0;
-    //while (vision == 0) vision = system("python3 vision.py");
+    float camera_height = 0.32;
+    int vision = 0;
+    while (vision == 0) vision = system("python3 vision.py");
     ifstream points_file("points.txt"); //Calibrated xy coordinates in robot base frame
     string num;
     int points[8][2][2]; //Part / cur-target / x-y
@@ -107,7 +108,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    int part = LEFT_SLEEVE;
+    int part = RIGHT_SLEEVE;
     int action = REACH;
     bool start = true;
 
@@ -129,15 +130,16 @@ int main(int argc, char** argv) {
     MatrixXd walle_N_prec = MatrixXd::Identity(dof, dof);
 
     // arm task (joints 0-6)
-    const string control_link = "link7";
-    const Vector3d control_point = Vector3d(0.195, -0.0, 0.0785);
+    const string control_link = "end-effector";
+    const Vector3d control_point = 0 * Vector3d(0.195, -0.0, 0.0785);
     Affine3d compliant_frame = Affine3d::Identity();
     compliant_frame.translation() = control_point;
 
     auto walle_pose_task = std::make_shared<Sai2Primitives::MotionForceTask>(walle, control_link, compliant_frame);
-    walle_pose_task->setPosControlGains(200, 20, 0);
-    walle_pose_task->setOriControlGains(200, 20, 0);
-    walle_pose_task->setDynamicDecouplingType(FULL_DYNAMIC_DECOUPLING);
+    walle_pose_task->setPosControlGains(400, 40, 0);
+    walle_pose_task->setOriControlGains(400, 40, 0);
+    // walle_pose_task->setDynamicDecouplingType(FULL_DYNAMIC_DECOUPLING);
+    walle_pose_task->setDynamicDecouplingType(BOUNDED_INERTIA_ESTIMATES);
 
     //walle_pose_task->enableInternalOtg();
     walle_pose_task->enableVelocitySaturation();
@@ -148,7 +150,7 @@ int main(int argc, char** argv) {
     // joint task for posture control
     auto walle_joint_task = std::make_shared<Sai2Primitives::JointTask>(walle);
     walle_joint_task->enableVelocitySaturation(M_PI / 8);
-    walle_joint_task->setGains(100, 20, 0);
+    walle_joint_task->setGains(100, 10, 0);
 
     // double kp_j = 200.0;
     // double kv_j = 20.0;
@@ -213,9 +215,6 @@ int main(int argc, char** argv) {
     InputParameter<3, EigenVector> input;
     OutputParameter<3, EigenVector> output;
 
-    // state machine
-    int state = POSTURE;
-
     while (runloop) {
         // wait for next scheduled loop
         timer.waitForNextLoop();
@@ -235,123 +234,161 @@ int main(int argc, char** argv) {
 
         if (!flag_simulation) {
             MatrixXd M = redis_client.getEigen(MASSMATRIX_KEY);
-            // M(4, 4) += 0.2;
-            // M(5, 5) += 0.2;
-            // M(6, 6) += 0.2;
             walle->updateModel(M);
-            // walle->updateModel();
 
         } else {
             walle->updateModel();
         }
 
-//         // update kinematic targets
-//         walle_ee_pos = walle->position(control_link, control_point);
-//         walle_ee_ori = walle->rotation(control_link);
-//         Matrix3d walle_R_desired = walle_ee_ori;
-//         Vector3d walle_x_desired = walle_ee_pos;
+        // update kinematic targets
+        walle_ee_pos = walle->position(control_link, control_point);
+        walle_ee_ori = walle->rotation(control_link);
+        Matrix3d walle_R_desired = walle_ee_ori;
+
+        // y-axis rotation of 120 degrees
+        Matrix3d rotation120;
+        rotation120 <<
+            -0.5, 0.0, sqrt(3.0)/2.0,
+            0.0, 1.0, 0.0,
+            sqrt(3.0)/-2.0, 0.0, -0.5;
+
+        Matrix3d rotation5;
+        rotation5 <<
+            cos(M_PI/36.0), 0, sin(M_PI/36.0),
+            0.0, 1.0, 0.0,
+            -1*sin(M_PI/36.0), 0, cos(M_PI/36.0);
+
+        Vector3d walle_x_desired = walle_ee_pos;
 
 
-//         Matrix3d ee_ori_world;
-//         Matrix3d box_ori = Matrix3d::Zero();
-//         Vector3d x_cur, x_target;
 
-//         // cout << "start's value: " << start << endl;
-//         // cout << "reach's value: " << REACH << endl;
+        Matrix3d ee_ori_world;
+        // Matrix3d box_ori = Matrix3d::Zero();
+        Vector3d x_cur, x_target;
 
-// // 
-//         // cout << "entered start" << endl;
-//         x_cur =  Vector3d(-0.0423415, -0.611336, 0.425331);
-//         // x_target = Vector3d(0.459529, 0.0571716, 0.358493);
-//         x_target = Vector3d(0.1, 0.1, 0.1);
-//         Matrix3d ori_cur;
-//         Matrix3d ori_target; 
-//         ori_cur <<  -0.80416, -0.355678,  0.476257,
-//                     -0.513481,  0.819289, -0.255152,
-//                     -0.29944, -0.449732, -0.841473;
-//         ori_target << -0.80416, -0.355678,  0.476257,
-//                     -0.513481,  0.819289, -0.255152,
-//                     -0.29944, -0.449732, -0.841473;
-
-        /*
-
-        new one =  0.459529
-        0.0571716
-        0.358493
-        -0.616993   0.655408  -0.435613
-        0.726577   0.687069 0.00463211
-        0.302333  -0.313649  -0.900122
-
-
-        0.653312
-        -0.00944733
-        0.258136
-        -0.552677  0.373262  0.745134
-        0.638464  0.764289 0.0907009
-        -0.535642  0.525869 -0.660719
-
-
-        0.532921
-        0.291814
-        0.328841
-        -0.855973 -0.0649666   0.512922
-        0.33835   0.679746   0.650741
-        -0.390933   0.730565  -0.559863
-        
-        */
     
-        if (state == POSTURE) {
+        if (start) {
+            if (part <= RIGHT_SLEEVE) {
+                x_cur = Vector3d(points[part][0][0], points[part][0][0], camera_height);
+                x_target = Vector3d(points[part][1][0], points[part][1][0], camera_height);
+            }
+            else {
+                x_cur = Vector3d(0.0, 0.0, 0.2); // move above workspace when done
+                x_target = Vector3d(0.1, 0.0, 0.2);
+                
+            }
+            // x_cur = Vector3d(0.5, -0.1, 0.2);
+            // x_target = Vector3d(0.5, 0.1, 0.2);
 
-            q_desired(0) = q_init(0) + 1e-1 * sin(2 * M_PI * time);
-            walle_joint_task->setGoalPosition(q_desired);
+            if (action == REACH) {
+                // walle_pose_task->setGoalPosition(x_cur);
+                walle_pose_task->setGoalOrientation(walle_ee_ori * rotation5 * walle_ee_ori.inverse() * ori(x_cur, x_target));
+            } else if (action == SLIP) {
+                walle_pose_task->parametrizeForceMotionSpaces(
+					1, Vector3d::UnitZ());
 
-            // walle_N_prec.setIdentity();
-            walle_joint_task->updateTaskModel(MatrixXd::Identity(7, 7));
-            walle_command_torques = 1e0 * walle_joint_task->computeTorques();
+				// set the force control
+				walle_pose_task->setGoalForce(-3.0 * Vector3d::UnitZ());
 
-            // if (walle_joint_task->goalPositionReached(1e-1)) {
-            //     std::cout << "Posture to Motion\n";
-            //     state = MOTION;
-            //     continue;
-            // }
+				walle_pose_task->setForceControlGains(35.0, 20.0, 0.0);
+                // walle_pose_task->setGoalPosition(x_target);
+                walle_pose_task->setGoalPosition(walle_ee_pos-Vector3d(0.0, 0.15, 0.5));
+                walle_pose_task->setGoalOrientation(walle_ee_ori * rotation5 * walle_ee_ori.inverse() * ori(x_cur, x_target));
+                
+            } else if (action == FLIP) {
+                // Keep end effector position fixed, only change orientation
+                walle_pose_task->setGoalPosition(walle_ee_pos);
+                walle_pose_task->parametrizeForceMotionSpaces(0);
+                walle_pose_task->setGoalOrientation(walle_ee_ori * rotation120 * walle_ee_ori.inverse() * ori(x_cur, x_target));
+            } else if (action == RISE) {
+                //walle_pose_task->parametrizeForceMotionSpaces(0);
+                // walle_pose_task->setGoalPosition(x_target + Vector3d(0, 0, 0.0));
+            }
 
-        } else if (state == MOTION) {
-
-            // cout << "entered reach" << endl;
-            walle_pose_task->setGoalPosition(walle->position(control_link, control_point));
-            //walle_pose_task->setGoalOrientation(ori_target);
-
-
-            // update task model
-            // cout << "EXIT" << endl;
-            walle_N_prec.setIdentity();
-            walle_pose_task->updateTaskModel(walle_N_prec);
-            walle_joint_task->updateTaskModel(walle_pose_task->getTaskAndPreviousNullspace());
-
-            // compute torques
-            // cout << "command torques BEFORE" << "\n";
-            // cout << walle_command_torques << "\n";
-            // cout << "currentposition AFTER" << "\n";
-            // cout << walle_ee_pos << "\n";
-
-            walle_command_torques = 0 * walle_joint_task->computeTorques() + 0 * walle_pose_task->computeTorques();
-
+            start = false;
+            if (part <= RIGHT_SLEEVE) {
+                cout << "Working on action " << action << " with part " << part << "\n";
+            } else {
+                cout << "Done!" << "\n";
+            }
+        } else if (walle_pose_task->goalPositionReached(0.05) && walle_pose_task->goalOrientationReached(0.05) && part <= THIRD2) {
+            cout << "Moving on! Yay!" << "\n";
+            action = (action + 1) % 4;
+            cout << "Action: " << action << endl;
+            if (action == 0) part += 1;
+            start = true;
         }
 
+        // if (action == LEFT) {
+        //     Vector3d goal_position = Vector3d(0.5, -0.1, 0.5);
+        //     if (start) {
+        //         walle_pose_task->setGoalPosition(goal_position);
+        //     }
+        //     // cout << "left" << endl;
+            
+
+        //     double error = (walle_ee_pos - goal_position).norm();
+        //     cout << "left error: " << error << endl;
+
+        //     start = false;
+
+        //     // cout << "in left" << endl;
+
+        //     if (error < 5e-2) {
+        //         cout << "switching to right" << endl;
+        //         action = RIGHT;
+        //         start = true;
+        //     }
+
+        // } else if (action == RIGHT) {
+        //     // cout << "right" << endl;
+        //     Vector3d goal_position = Vector3d(0.5, 0.1, 0.5);
+        //     if (start) walle_pose_task->setGoalPosition(goal_position);
+
+        //     double error = (walle_ee_pos - goal_position).norm();
+        //     cout << "right error: " << error << endl;
+
+        //     start = false;
+
+        //     // cout << "in right" << endl;
+
+        //     if (error < 5e-2) {
+        //         cout << "switching to left" << endl;
+        //         action = LEFT;
+        //         start = true;
+        //     }
+        // }
+
+        // Vector3d goal_position = Vector3d(0.5, -0.1, 0.5);
+        // walle_pose_task->setGoalPosition(goal_position);
+
+        // Matrix3d goal_ori;
+        // goal_ori << 1, 0, 0,
+        //             0, 1, 0,
+        //             0, 0, 1;
+        
+        // walle_pose_task->setGoalOrientation(goal_ori);
+
+        // update task model
+        walle_N_prec.setIdentity();
+        walle_pose_task->updateTaskModel(walle_N_prec);
+        walle_joint_task->updateTaskModel(walle_pose_task->getTaskAndPreviousNullspace());
+
+        // compute torques
+        walle_command_torques = walle_joint_task->computeTorques() + walle_pose_task->computeTorques();
+
         // cout << "command torques AFTER" << "\n";
-        cout << walle_command_torques.transpose() << "\n";
+        // cout << walle_command_torques.transpose() << "\n";
         // cout << "currentposition AFTER" << "\n";
         // cout << walle_ee_pos << "\n";
 
-        
+        // cout << walle_ee_ori << endl;
 
         // send to redis
         // redis_client.sendAllFromGroup();
         // cout << "sent it to redis" << endl;
 
         redis_client.setEigen(JOINT_TORQUES_COMMANDED_WALLE_KEY, walle_command_torques);
-        start = false;
-
     }
 
     cout << "reached outside" << endl;
--- vision.py
+++ vision.py
@@ -66,11 +66,36 @@ def find_sleeve(mask, bottom, sleeve_top):
     sleeve_mid_inner = (x_mid, bottom[1])
     return sleeve_mid_outer, sleeve_mid_inner
 
+def order_points(pts):
+    # sort the points based on their x-coordinates
+    xSorted = pts[np.argsort(pts[:, 0]), :]
+    # grab the left-most and right-most points from the sorted
+    # x-roodinate points
+    leftMost = xSorted[:2, :]
+    rightMost = xSorted[2:, :]
+    # now, sort the left-most coordinates according to their
+    # y-coordinates so we can grab the top-left and bottom-left
+    # points, respectively
+    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+    (tl, bl) = leftMost
+    # now that we have the top-left coordinate, use it as an
+    # anchor to calculate the Euclidean distance between the
+    # top-left and right-most points; by the Pythagorean
+    # theorem, the point with the largest distance will be
+    # our bottom-right point
+    D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+    (br, tr) = rightMost[np.argsort(D)[::-1], :]
+    # return the coordinates in top-left, top-right,
+    # bottom-right, and bottom-left order
+    return np.array([tl, tr, br, bl], dtype="float32")
+
 def projection(green_mask, markers):
     width, height = green_mask.shape
-    dest_markers = np.float32([[0,height], [0.9*width, height], [0.9*width, 0], [0, 0]])
-    M = cv2.getPerspectiveTransform(np.float32(markers), dest_markers)
-    return cv2.warpPerspective(green_mask, M, (width, height))
+    markers = order_points(np.array(markers))
+    #dest_markers = np.float32([[0,0.0*width], [0.0*height, 0.7*width], [0.9*height, width], [0.7*height, 0.0*width]])
+    dest_markers = np.float32([[0,0], [1.0*height, 0.0*width], [height, width], [0.0*height, 1.0*width]])
+    M = cv2.getPerspectiveTransform(markers, dest_markers)
+    return cv2.warpAffine(green_mask, M, (height, width))
 
 def barycentric_weights(x1, y1, x2, y2, x3, y3, x4, y4, xn, yn):
     def area(x1, y1, x2, y2, x3, y3):
@@ -100,29 +125,42 @@ def convert_to_world_coords(mask, point):
     physical_points = np.array([[0.252, -0.527], [0.252, 0.463], [0.925, 0.463], [0.925, -0.527]])
     weights = barycentric_weights(x1, y1, x2, y2, x3, y3, x4, y4, point[0], point[1])
 
-    world_coord = np.zeros(2)
+    world_coord = np.zeros(3)
     for i in range(4):
         world_coord += physical_points[i]*weights[i]
     
     return world_coord
 
+def get_rgb(event, x, y, flags, param):
+    global clicked_rgb
+    if event == cv2.EVENT_LBUTTONDOWN:
+        clicked_rgb = param[y, x, :]
+        print(f"Clicked pixel RGB values: {clicked_rgb}")
+
 class WebcamProcessor:
     def __init__(self):
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture("http://admin:admin@10.5.119.51:8081/video")
         self.event = Event()
         self.start_processing = False
 
     def process_stream(self):
-        lower_color_red = np.array([0, 150, 150])  # Example lower HSV threshold for red
-        upper_color_red = np.array([25, 220, 255])  # Example upper HSV threshold for red
+        lower_color_red = np.array([0, 51, 150])  # Example lower HSV threshold for red
+        upper_color_red = np.array([5, 220, 255])  # Example upper HSV threshold for red
 
         lower_color = np.array([35, 100, 100])  # Example lower HSV threshold for green
-        upper_color = np.array([85, 255, 255])  # Example upper HSV threshold for green
+        upper_color = np.array([85, 255, 200])  # Example upper HSV threshold for green
 
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             if not ret:
                 break
+            
+            cv2.namedWindow('Webcam Frame')
+            cv2.setMouseCallback('Webcam Frame', get_rgb, frame)
+
+            frame = frame[:1000, :1100]
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
             if self.start_processing:
                 mask_red = detect_by_color(frame, lower_color_red, upper_color_red)
@@ -132,6 +170,7 @@ class WebcamProcessor:
                 lefttop_red = find_left_top_corner(mask_red)
                 righttop_red = find_right_top_corner(mask_red)
                 points_red = [lefttop_red, righttop_red, rightbottom_red, leftbottom_red]
+
 
                 mask = detect_by_color(frame, lower_color, upper_color)
                 mask = projection(mask, points_red)
@@ -150,8 +189,8 @@ class WebcamProcessor:
                 rightbottom_target = (int(rightbottom[0]-shirt_height/6), int(rightbottom[1]-0.25*shirt_width))
 
                 midbottom = (int((leftbottom[0]+rightbottom[0])/2), int((leftbottom[1]+rightbottom[1])/2))
-                mid2 = (midbottom[0], midbottom[1])
-                mid3 = (midbottom[0]-shirt_height/3, midbottom[1])
+                mid2 = (int(midbottom[0]-shirt_height/3), midbottom[1])
+                mid3 = (int(midbottom[0]-2*shirt_height/3), midbottom[1])
 
                 third = (left_sleeve_inner[0], left_sleeve_inner[1]+shirt_width/3)
                 third2 = (left_sleeve_inner[0], left_sleeve_inner[1]+2*shirt_width/3)
@@ -161,7 +200,10 @@ class WebcamProcessor:
                             mid2, mid2, mid3, left_sleeve_inner, third, third, third2]
 
                 for point in points:
-                    cv2.circle(mask, (int(point[1]), int(point[0])), 50, (0, 0, 255), -1)
+                    cv2.circle(mask, (int(point[1]), int(point[0])), 25, (0, 0, 255), -1)
+                    cv2.circle(frame, (int(point[1]), int(point[0])), 25, (0, 0, 255), -1)
+                for i in range(len(points_red)):
+                    cv2.circle(frame, (int(points_red[i][1]), int(points_red[i][0])), 25, (255, 70*i, 0), -1)
 
                 # Display both the original frame and the mask
                 combined_image = np.hstack((frame, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)))
